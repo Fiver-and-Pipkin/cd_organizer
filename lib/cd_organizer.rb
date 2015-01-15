eaec1d3379ec5a409e4c08f3b3b969bf899172f0
debbie Cravey & Marc Lee <student@epicodus.com>
2015-01-15 11:32:17 -0800

    add method and spec to search album by id

--- lib/cd_organizer.rb
+++ lib/cd_organizer.rb
@@ -5,14 +5,43 @@ class Library
   define_method(:initialize) do |attributes|
     @album = attributes.fetch(:album)
     @artist = attributes.fetch(:artist)
+    @id = @@cds.length().+(1)
   end
 
-    define_singleton_method(:all) do
+    define_method(:id) do
+    @id
+  end
+
+  define_singleton_method(:all) do
     @@cds
   end
 
   define_singleton_method(:clear) do
     @@cds = []
+  end
+
+  define_singleton_method(:find) do |identification|
+    found_album = nil
+    @@cds.each() do |cd|
+      if cd.id().eql?(identification.to_i())
+        found_album = cd
+    end
+  end
+  found_album
+end
+
+    define_singleton_method(:search) do |cd|
+      sought_album = "album not found."
+      sought_artist = nil
+      searched_album = []
+        @@cds.each() do |record|
+          if record.album() == cd
+            sought_album = record.album()
+            sought_artist = record.artist()
+          end
+        end
+      searched_album = [sought_album, sought_artist]
+      searched_album
   end
 
     define_method(:save) do
--- spec/cd_organizer_spec.rb
+++ spec/cd_organizer_spec.rb
@@ -27,6 +27,13 @@ describe(Library) do
   end
 end
 
+  describe('#id') do
+    it("returns the id of the album") do
+      test_album = Library.new({:album => "Greatest Hits", :artist => "The Doors"})
+      expect(test_album.id()).to eq(1)
+    end
+  end
+
   describe('#save') do
     it("adds an album to the array of saved albums") do
     test_album = Library.new({:album => "Greatest Hits", :artist => "The Doors"})
@@ -48,4 +55,27 @@ end
       expect(Library.all()).to eq([])
     end
   end
+
+  describe('.find') do
+    it("returns a cd by its id number") do
+      test_album = Library.new({:album => "Greatest Hits", :artist => "The Doors"})
+      test_album.save()
+      test_album2 = Library.new({:album => "The White Album", :artist => "The Beatles"})
+      test_album2.save()
+      expect(Library.find(test_album.id())).to eq(test_album)
+    end
+  end
+
+    describe(".search") do
+    it("returns the Album name and Artist name") do
+      test_album2 = Library.new({:album => "The White Album", :artist => "The Beatles"})
+      test_album2.save()
+      test_album = Library.new({:album => "Greatest Hits", :artist => "The Doors"})
+      test_album.save()
+      expect(Library.search("Greatest Hits")).to(eq([test_album.album(),test_album.artist()]))
+    end
+  end
+
+
+
 end
